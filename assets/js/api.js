41eec6d220050c31f7e2a14ac8f6882a29821edb
Carlos Augusto Rodrigues Teixeira <carlos1818teixeira@gmail.com>
2023-07-31 13:32:46 -0300

    Corrigir problemas no arquivo api.js e atualizar profile.json

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = '';
+    const url = 'https://raw.githubusercontent.com/carlos1818augusto/Portifolio--Carlos-Augusto./main/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
